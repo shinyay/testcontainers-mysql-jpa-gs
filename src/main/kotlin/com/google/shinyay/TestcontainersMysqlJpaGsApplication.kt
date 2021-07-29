615311cc5d4ccd74eda70f8f159b3da6e09e8137
shinyay <shinya.com@gmail.com>
2021-07-29 17:32:07 +0900

    Initial Commit

---
+++ src/main/kotlin/com/google/shinyay/TestcontainersMysqlJpaGsApplication.kt
@@ -0,0 +1,11 @@
+package com.google.shinyay
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TestcontainersMysqlJpaGsApplication
+
+fun main(args: Array<String>) {
+	runApplication<TestcontainersMysqlJpaGsApplication>(*args)
+}
