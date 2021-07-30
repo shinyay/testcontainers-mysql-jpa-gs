91f9fc0d97aabc81e5f1931f9ba47d33034df16a
shinyay <shinya.com@gmail.com>
2021-07-30 17:32:31 +0900

    Add test container for MySQL configuration as static

--- src/test/kotlin/com/google/shinyay/testmysql/TestContainerForBook.kt
+++ src/test/kotlin/com/google/shinyay/testmysql/TestContainerForBook.kt
@@ -4,6 +4,8 @@ import com.google.shinyay.repository.BookRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.DynamicPropertyRegistry
+import org.springframework.test.context.DynamicPropertySource
 import org.testcontainers.containers.MySQLContainer
 import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
@@ -13,11 +15,20 @@ import org.testcontainers.junit.jupiter.Testcontainers
 @AutoConfigureMockMvc
 class TestContainerForBook {
 
-    @Container
-    val database = MySQLContainer<Nothing>("mysql:5.7.33").apply {
-        withDatabaseName("myapp")
-    }
-
     @Autowired
     lateinit var repository: BookRepository
+
+    companion object {
+        @Container
+        val database = MySQLContainer<Nothing>("mysql:5.7.33").apply {
+            withDatabaseName("myapp")
+        }
+
+        @JvmStatic
+        @DynamicPropertySource
+        fun properties(registry: DynamicPropertyRegistry) {
+            registry.add("spring.datasource.url", database::getJdbcUrl)
+            registry.add("spring.jpa.hibernate.ddl-auto") { "create-drop" }
+        }
+    }
 }
