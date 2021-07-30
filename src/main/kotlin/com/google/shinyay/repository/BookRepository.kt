ea28288ffe9d70d73af2a55be0522aeafeb35aac
shinyay <shinya.com@gmail.com>
2021-07-30 10:08:32 +0900

    Add Book jpa repository

---
+++ src/main/kotlin/com/google/shinyay/repository/BookRepository.kt
@@ -0,0 +1,9 @@
+package com.google.shinyay.repository
+
+import com.google.shinyay.entity.Book
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface BookRepository : JpaRepository<Book, Long> {
+}
