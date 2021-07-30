f02d890353d007c9286f8ffd00d4440e54748144
shinyay <shinya.com@gmail.com>
2021-07-30 17:32:02 +0900

    Add Post to register books

--- src/main/kotlin/com/google/shinyay/controller/BookController.kt
+++ src/main/kotlin/com/google/shinyay/controller/BookController.kt
@@ -1,8 +1,20 @@
 package com.google.shinyay.controller
 
+import com.google.shinyay.entity.Book
 import com.google.shinyay.service.BookService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("/api/v1")
 class BookController(val service: BookService) {
+
+    @PostMapping("/books")
+    fun registerBook(@RequestBody book: Book): ResponseEntity<Book> {
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.repository.save(book))
+    }
 }
