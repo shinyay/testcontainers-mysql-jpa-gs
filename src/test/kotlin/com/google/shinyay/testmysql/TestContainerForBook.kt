8e17c2fd454140042e33fa8ae9b9cfc690926da4
shinyay <shinya.com@gmail.com>
2021-07-30 14:28:56 +0900

    Add test annotation for MocMvc

--- src/test/kotlin/com/google/shinyay/testmysql/TestContainerForBook.kt
+++ src/test/kotlin/com/google/shinyay/testmysql/TestContainerForBook.kt
@@ -2,11 +2,15 @@ package com.google.shinyay.testmysql
 
 import com.google.shinyay.repository.BookRepository
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
 import org.testcontainers.containers.MySQLContainer
 import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
 
 @Testcontainers
+@SpringBootTest
+@AutoConfigureMockMvc
 class TestContainerForBook {
 
     @Container
