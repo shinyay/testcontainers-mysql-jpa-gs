6a92788b0898fa45f419cf88d764051cc0b0122f
shinyay <shinya.com@gmail.com>
2021-07-30 16:59:06 +0900

    Add table name

--- src/main/kotlin/com/google/shinyay/entity/Book.kt
+++ src/main/kotlin/com/google/shinyay/entity/Book.kt
@@ -1,11 +1,9 @@
 package com.google.shinyay.entity
 
-import javax.persistence.Entity
-import javax.persistence.GeneratedValue
-import javax.persistence.GenerationType
-import javax.persistence.Id
+import javax.persistence.*
 
 @Entity
+@Table(name = "books")
 data class Book(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
