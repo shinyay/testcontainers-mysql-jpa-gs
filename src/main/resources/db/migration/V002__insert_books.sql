689ab1886620c95f411e59db1894a9ae5ce32a95
shinyay <shinya.com@gmail.com>
2021-08-02 13:21:54 +0900

    Add test data

--- src/main/resources/db/migration/V002__insert_books.sql
+++ src/main/resources/db/migration/V002__insert_books.sql
@@ -1,1 +1,3 @@
-INSERT INTO test.books VALUES (1, '978-4-7710-1061-3', 'Spring', 'shinyay', 1000)
+INSERT INTO test.books VALUES (1, '978-4-7710-1061-3', 'Spring', 'shinyay', 1000);
+INSERT INTO test.books VALUES (2, '978-4-7710-1062-3', 'Java', 'yanashin', 980);
+INSERT INTO test.books VALUES (3, '978-4-7710-1063-3', 'GCP', 'shinyay', 500);
