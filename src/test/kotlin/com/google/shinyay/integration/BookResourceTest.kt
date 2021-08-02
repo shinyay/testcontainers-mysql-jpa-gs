073364ba91cc60238a241673513387302bd24cdf
shinyay <shinya.com@gmail.com>
2021-08-02 16:02:26 +0900

    Add json check by jsonpath

--- src/test/kotlin/com/google/shinyay/integration/BookResourceTest.kt
+++ src/test/kotlin/com/google/shinyay/integration/BookResourceTest.kt
@@ -1,15 +1,19 @@
 package com.google.shinyay.integration
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
 import com.google.shinyay.entity.Book
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import org.testcontainers.junit.jupiter.Testcontainers
 
@@ -34,5 +38,14 @@ class BookResourceTest {
             .content(json))
             .andDo(MockMvcResultHandlers.print())
             .andExpect(status().isCreated)
+        mockMvc.perform(get("/api/v1/books/978-4-7710-1067-3"))
+            .andDo(MockMvcResultHandlers.print())
+            .andExpect(status().isOk)
+            .andExpect(jsonPath("$.length()").value(1))
+            .andExpect(jsonPath("$[0].title").exists())
+            .andExpect(jsonPath("$[0].title").value("test"))
+
+
+
     }
 }
