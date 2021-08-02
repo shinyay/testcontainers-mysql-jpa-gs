b3336fdf73213f32718741e2748a8f1f209e2b73
shinyay <shinya.com@gmail.com>
2021-08-02 13:50:45 +0900

    Add find by isbn function

--- src/main/kotlin/com/google/shinyay/service/BookService.kt
+++ src/main/kotlin/com/google/shinyay/service/BookService.kt
@@ -1,8 +1,15 @@
 package com.google.shinyay.service
 
+import com.google.shinyay.entity.Book
+import com.google.shinyay.logger
 import com.google.shinyay.repository.BookRepository
 import org.springframework.stereotype.Service
 
 @Service
 class BookService(val repository: BookRepository) {
+
+    fun findBookByIsbn(isbn: String): MutableList<Book> {
+        logger.info("[ISBN]: $isbn")
+        return repository.findBookByIsbn(isbn)
+    }
 }
