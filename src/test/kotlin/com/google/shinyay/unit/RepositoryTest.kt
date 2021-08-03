0cd21783e353460ac647cfab1c690bcf370cf484
shinyay <shinya.com@gmail.com>
2021-08-03 17:19:29 +0900

    Add Testcontainer annotation

--- src/test/kotlin/com/google/shinyay/unit/RepositoryTest.kt
+++ src/test/kotlin/com/google/shinyay/unit/RepositoryTest.kt
@@ -6,7 +6,9 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.testcontainers.junit.jupiter.Testcontainers
 
+@Testcontainers
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class RepositoryTest {
