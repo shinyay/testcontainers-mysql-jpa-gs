94e92cb4c7bee3b65e919d5f607b872d626a0195
shinyay <shinya.com@gmail.com>
2021-08-04 06:44:14 +0900

    Add find all books test

--- src/test/kotlin/com/google/shinyay/unit/RepositoryTest.kt
+++ src/test/kotlin/com/google/shinyay/unit/RepositoryTest.kt
@@ -3,6 +3,7 @@ package com.google.shinyay.unit
 import com.google.shinyay.repository.BookRepository
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertAll
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
@@ -21,5 +22,16 @@ class RepositoryTest {
         val result = repository.findAll()
 
         assertThat(result.count()).isEqualTo(3)
+    }
+
+    @Test
+    fun findAllBooksShouldReturnAllBooks() {
+        val result = repository.findAll()
+
+
+
+        assertThat(result[0].title).isEqualTo("Spring")
+        assertThat(result[1].title).isEqualTo("Java")
+        assertThat(result[2].title).isEqualTo("GCP")
     }
 }
